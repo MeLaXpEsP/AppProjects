4ac4e532ddfcb403996830b946403b6877acaf02
MeLaXpEsP <167686124+MeLaXpEsP@users.noreply.github.com>
2024-09-22 09:04:00 +0000

    Ejercicio 3 finalizado

--- Práctica2 sintaxis/Ejercicio 3 Numeros.js
+++ Práctica2 sintaxis/Ejercicio 3 Numeros.js
@@ -1,10 +1,8 @@
-var vecesImpreso = 5;
-var numeroMultiplicado = 10;
-
-
 function imprimir(vecesImpreso, numeroMultiplicado){
     for(let i = 0; i < vecesImpreso ; i++){
         console.log(numeroMultiplicado);
         numeroMultiplicado *= 2;
     }
 }
+
+imprimir(4, 6);
