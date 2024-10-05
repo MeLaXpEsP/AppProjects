778a1a2275cde59c13fea1dd16289590406bd6c4
MeLaXpEsP <167686124+MeLaXpEsP@users.noreply.github.com>
2024-10-05 18:58:14 +0000

    Ejercicio 5

---
+++ DEC - Desarrollo entorno cliente/Práctica 3/2.05 Jugando con la sintaxis/Ejercicio 1.js
@@ -0,0 +1,39 @@
+"use strict";
+
+var valorFinal = [];
+
+/*Funcion para llenar array con 9 numeros*/
+function llenarArray() {
+
+    let numeros = [];
+
+    /*Genera los 9 númeoros mediante un bucle for y un do while por si se repite algun número no avanzar y volver a generarlo*/
+    for (let i = 0; i < 9; i++) {
+        let numeroGenerado;
+        do {
+            numeroGenerado = Math.floor(Math.random() * 9) + 1;
+        } while (numeros.includes(numeroGenerado));
+        numeros.push(numeroGenerado);
+    }
+    console.log(numeros);
+    return numeros;
+}
+
+function comprobarNumeros(array) {
+    var contador = {};
+    for (var i = 0; i < array.length; i++) {
+        /*Comprueba si en "contador" existe el numero de array[i], si es si, devuelve true*/
+        if (contador[array[i]]) {
+            return true; 
+        }
+        /*Almacena el valor */
+        contador[array[i]] = true; 
+    }
+
+    /*Si no encuentra coincidencias devuelve false */
+    return false;
+}
+
+
+
+comprobarNumeros(llenarArray());
