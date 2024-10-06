903ee5a51313eb32f156d709a945bcbae91acb00
MeLaXpEsP <167686124+MeLaXpEsP@users.noreply.github.com>
2024-10-06 10:54:50 +0000

    ejercicios

---
+++ Testeo/Componentes.js
@@ -0,0 +1,17 @@
+/*Estructura básica de un componente
+function Componente(props){
+    //Lógica del componente -> Aquí está la logica y comportamiento en código JS
+    return(
+        //Vista del componente(JSX) -> Esta parte se muestra en el navegador con código JSX
+    );
+*/
+
+//Ejemplo 
+
+function basico(props){
+    return(
+        <div>
+            DIV vacío
+        </div>
+    )
+}
